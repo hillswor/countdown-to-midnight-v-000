933e2557456474de27fce5743ac02dcaaf26e47c
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-09-17 17:42:00 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
-def countdown
-  timer = 10
+def countdown(seconds)
+  timer = seconds
   while timer > 0
     puts "#{timer}"
     timer -= 1
   end
-  puts "Happy New Year!"
+  puts "HAPPY NEW YEAR!"
 end
 
 countdown
